1051c2eb1b0e1f5a2b3ef1bba36740ab61b1c556
MexpressoBean <kingk102@gmail.com>
2022-01-02 22:33:27 -0800

    added content to root path

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV['EXECJS_RUNTIME'] = 'Node'  # make the ExecJs use NodeJs
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
