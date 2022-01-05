4e7cd41308f601dc677fb210488f6be6068b5122
MexpressoBean <kingk102@gmail.com>
2022-01-04 22:51:51 -0800

    built out some more functionality for auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,20 @@ class SessionsController < ApplicationController
     end
 
     def create
-        
+        student = Student.find_by(email: params[:sessions][:email].downcase)
+        if student && student.authenticate(params[:sessions][:password])
+            session[:student_id] = student.id
+            flash[:notice] = "Successful log in"
+            redirect_to student
+        else
+            flash.now[:notice] = "Something was wrong with the information"
+            render 'new'
+        end
     end
 
     def destroy
-        
+        session[:student_id] = nil
+        flash[:notice] = "Successful log out"
+        redirect_to root_path
     end
 end
