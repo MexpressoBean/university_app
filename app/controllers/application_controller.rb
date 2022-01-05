f8cb0ab79e29fb37d0de995346a66f13eaaa9b30
MexpressoBean <kingk102@gmail.com>
2022-01-04 23:19:32 -0800

    added restrictions from view and controller level to enforce user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?
+    before_action :require_user
 
     def logged_in?
         !!current_user
+    end
+
+    def require_user
+        if !logged_in?
+            flash[:notice] = "You must be logged in to preform that action"
+            redirect_to login_path
+        end
     end
 
     def current_user
